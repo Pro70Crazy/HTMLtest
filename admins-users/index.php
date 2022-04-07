3827d827dd63a91c7a09f7f5e578e33a01172fa9
Pro70 <69443748+Pro70Crazy@users.noreply.github.com>
2022-04-07 11:06:46 +0700

    Create index.php

---
+++ admins-users/index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Admins VS Users</title>
+  </head>
+  <body>
+    <h3>Login please</h3>
+    <p>HINT for Admin. Ordinary users can login with any username and password: Username: Admin Password: Password123</p>
+    <form method="post" action="/HTMLtest/admins-users/login.php">
+      <p>Username: <input type="text" name="UserName" size="20"></p>
+      <p>Password: <input type="password" name="UserPassword" size="20"></p>
+      <p><input type="submit" value="Login" name="B1"><input type="reset" value="Reset" name="B2"></p>
+  </body>
+</html>
